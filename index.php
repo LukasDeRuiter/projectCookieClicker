97e6a1d0fa9a8158507c34be20a05831abed85fa
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-11 14:35:31 +0200

    added unitcards, cookie image and js file

--- index.php
+++ index.php
@@ -5,10 +5,16 @@
 
     <div class="mainContainer">
         <div class="cookieContainer">
+            <img src="assets/cookie1.png" class="cookie">
 
         </div>
         
         <div class="unitContainer">
+            <div class="singleUnitCard"></div>
+            <div class="singleUnitCard"></div>
+            <div class="singleUnitCard"></div>
+            <div class="singleUnitCard"></div>
+            <div class="singleUnitCard"></div>
 
         </div>
     </div>
