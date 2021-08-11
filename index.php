97981e8cd994bd687b1deae9f7410494ae321552
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-11 14:25:50 +0200

    added containers for cookie and units

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('templates/header.php'); ?>
+
+    <div class="mainContainer">
+        <div class="cookieContainer">
+
+        </div>
+        
+        <div class="unitContainer">
+
+        </div>
+    </div>
+
+
+
+
+<?php include('templates/footer.php'); ?>
+</html>
