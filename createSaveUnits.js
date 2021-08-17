bb5f6052caa141e0e2a7d6487a731b0b3117d6be
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-17 10:33:10 +0200

    loading now completly works!

--- createSaveUnits.js
+++ createSaveUnits.js
@@ -2,10 +2,10 @@ let units = [];
 
 for(let i = 0; i < 3 ; i++) {
     let loadedName = document.getElementById(`unitProperty1${i}`).value;
-    let loadedCookies = document.getElementById(`unitProperty2${i}`).value;
-    let loadedCost = document.getElementById(`unitProperty3${i}`).value;
-    let loadedAmount = document.getElementById(`unitProperty4${i}`).value;
-    let loadedIntervalTime = document.getElementById(`unitProperty5${i}`).value;
+    let loadedCookies = parseInt(document.getElementById(`unitProperty2${i}`).value);
+    let loadedCost = parseInt(document.getElementById(`unitProperty3${i}`).value);
+    let loadedAmount = parseInt(document.getElementById(`unitProperty4${i}`).value);
+    let loadedIntervalTime = parseInt(document.getElementById(`unitProperty5${i}`).value);
     units[i] = new Unit(loadedName, loadedCookies, loadedCost, loadedAmount, loadedIntervalTime);
 }
 
