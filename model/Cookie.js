983b46a875d2e1e01f81e7936bb70270da2a17e2
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-19 12:59:01 +0200

    added cookie getters and setters

--- model/Cookie.js
+++ model/Cookie.js
@@ -1,8 +1,34 @@
 class Cookie {
 
     //constructor
-    constructor(clickPower, image) {
+    constructor(clickPower, image, level) {
         this.clickPower = clickPower;
+        this.level = level;
+        this.image = image;
+    }
+
+    //getters and setters
+    getClickPower() {
+        return this.clickPower;
+    }
+
+    setClickPower(clickPower) {
+        this.clickPower = clickPower;
+    }
+
+    getLevel() {
+        return this.level;
+    }
+
+    setLevel(level) {
+        this.level = level;
+    }
+
+    getImage() {
+        return this.image;
+    }
+
+    setImage(image) {
         this.image = image;
     }
 
