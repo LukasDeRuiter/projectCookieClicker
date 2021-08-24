95739c56a63a9aabd9bdf9a8568bc75eed3cf771
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-24 09:39:45 +0200

    added getters and setters for upgrade class

--- model/Upgrade.js
+++ model/Upgrade.js
@@ -4,16 +4,64 @@ class Upgrade {
     nextID = 0;
 
     //constructor
-    constructor(name, description, cost, unitNumber) {
+    constructor(name, description, cost, isBought, unitNumber) {
         this.name = name;
         this.description = description;
         this.cost = cost;
         this.unitNumber = unitNumber;
+        this.isBought = isBought;
         this.upgradeID = nextID++;
     }
 
     //Getters and setters
 
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    getCost() {
+        return this.cost;
+    }
+
+    setCost(cost) {
+        this.cost = cost;
+    }
+
+    getUnitNumber() {
+        return this.unitNumber;
+    }
+
+    setUnitNumber(unitNumber) {
+        this.unitNumber = unitNumber;
+    }
+
+    getIsBought() {
+        return this.isBought;
+    }
+
+    setIsBought(isBought) {
+        this.isBought = isBought;
+    }
+
+    getUpgradeID() {
+        return this.upgradeID;
+    }
+
+    setUpgradeID(upgradeID) {
+        this.upgradeID = upgradeID;
+    }
 
     //Methods
 }
