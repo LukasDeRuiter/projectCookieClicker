33ea0da7dd8edb6804d90ffe869d69aa7d90627b
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-26 15:35:35 +0200

    randomcookie now works visually, only need to add code to allow bonus cookies

--- model/RandomCookie.js
+++ model/RandomCookie.js
@@ -24,8 +24,29 @@ class RandomCookie {
     appearOnRandomElement() {
             let chosenElement = Math.floor(Math.random() * 3);
             let ChosenElementID = `randomCookie${chosenElement}`;
+
+            let chosenElementLeft = Math.floor(Math.random() * 100);
+            let chosenElementTop = Math.floor(Math.random() * 100);
+
+            if(chosenElementLeft < 15) {
+                chosenElementLeft = 15;
+            } else if(chosenElementLeft > 85) {
+                chosenElementLeft = 85;
+            }
+
+            if(chosenElementTop < 15) {
+                chosenElementTop = 15;
+            } else if(chosenElementTop > 85) {
+                chosenElementTop = 85;
+            }
+
+            document.getElementById(`${ChosenElementID}`).style.left = `${chosenElementLeft}%`;
+            document.getElementById(`${ChosenElementID}`).style.top = `${chosenElementTop}%`;
             document.getElementById(`${ChosenElementID}`).style.display = "flex";
+
+
             this.cookieActive = true;
+
     }
 
     disappearOnAllElements() {
