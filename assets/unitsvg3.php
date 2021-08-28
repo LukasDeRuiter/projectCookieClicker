dc78fbdba2e873adc1835e6b4cb35dc5113beb19
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-28 11:24:48 +0200

    added new unit svg

---
+++ assets/unitsvg3.php
@@ -0,0 +1,150 @@
+<svg
+   width="210mm"
+   height="297mm"
+   viewBox="0 0 210 297"
+   version="1.1"
+   id="svg5"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs
+     id="defs2" />
+  <g
+     id="layer1">
+    <ellipse class="head13"
+       style="fill:#002e46;stroke:#000000;stroke-width:0"
+       id="path846"
+       cx="105.38772"
+       cy="104.32962"
+       rx="86.341743"
+       ry="83.167419" />
+    <ellipse class="eye13"
+       style="fill:#ffffff;stroke:#000000;stroke-width:0"
+       id="path1211-3"
+       cx="138.82399"
+       cy="81.262817"
+       rx="20.104086"
+       ry="13.332183" />
+    <path class="body13"
+       style="fill:#002e46;stroke:#000000;stroke-width:0"
+       id="path846-4"
+       d="m 17.354322,297.63435 a 86.341743,171.20215 0 0 1 42.965928,-148.97193 86.341743,171.20215 0 0 1 86.5485,-0.23029 86.341743,171.20215 0 0 1 43.16742,148.74275 l -86.34141,-0.48091 z" />
+    <ellipse class="eye23"
+       style="fill:#ffffff;stroke:#000000;stroke-width:0"
+       id="path1211"
+       cx="62.00523"
+       cy="81.474449"
+       rx="20.104086"
+       ry="13.332183" />
+    <ellipse class="eyelid13"
+       style="fill:#000000;stroke:#000000;stroke-width:0"
+       id="path1211-3-1"
+       cx="55.44495"
+       cy="83.167419"
+       rx="4.8673067"
+       ry="4.23244" />
+    <ellipse class="eyelid23"
+       style="fill:#000000;stroke:#000000;stroke-width:0"
+       id="path1211-3-1-7"
+       cx="133.74507"
+       cy="82.109314"
+       rx="4.8673067"
+       ry="4.23244" />
+    <ellipse class="mouth13"
+       style="fill:#800000;stroke:#000000;stroke-width:0"
+       id="path1443"
+       cx="99.673935"
+       cy="137.97749"
+       rx="35.340862"
+       ry="4.6556826" />
+    <rect class="eyebrow13"
+       style="fill:#000000;stroke:#000000;stroke-width:0"
+       id="rect1610"
+       width="64.333069"
+       height="12.274072"
+       x="-23.72513"
+       y="64.788162"
+       ry="2.5603464"
+       transform="rotate(-32.561804)" />
+    <rect class="eyebrow23"
+       style="fill:#000000;stroke:#000000;stroke-width:0"
+       id="rect1610-4"
+       width="64.333069"
+       height="12.274072"
+       x="125.30807"
+       y="-51.499222"
+       ry="2.5603464"
+       transform="rotate(36.002129)" />
+  </g>
+</svg>
+
+
+
+<style>
+    .body13 , .head13 {
+        animation: idle13 6s infinite alternate;
+    }
+
+    .eye13, .eye23, .mouth13 {
+        animation: idle23 6s infinite alternate;
+    }
+
+    .eyelid13, .eyelid23 {
+        animation: idle33 6s infinite alternate;
+    }
+
+    .eyebrow13 {
+        animation: idle43 6s infinite alternate;
+    }
+
+    .eyebrow23 {
+        animation: idle53 6s infinite alternate;
+    }
+
+    @keyframes idle13 {
+        0% {transform: translateX(0rem);}
+        25% {transform: translateX(1rem);}
+        50% {transform: translateX(2rem);}
+        75% {transform: translateX(-1rem);}
+        100% {transform: translateX(0rem);}
+    }
+
+    @keyframes idle23 {
+        0% {transform: translate(0rem, 0rem);}
+        25% {transform: translate(1rem, 0.5rem);}
+        40% {transform: translate(1rem, -0.5rem);}
+        50% {transform: translate(2rem, 0.5rem);}
+        60% {transform: translate(2rem, 1rem);}
+        75% {transform: translate(-1rem, -0.5rem);}
+        100% {transform: translate(0rem, 0rem);}
+    }
+
+    @keyframes idle33 {
+        0% {transform: translate(0rem, 0rem);}
+        25% {transform: translate(1.2rem, 0.7rem);}
+        40% {transform: translate(1rem, -0.7rem);}
+        50% {transform: translate(2.3rem, 0.7rem);}
+        60% {transform: translate(2.1rem, 1.2rem);}
+        75% {transform: translate(-1.2rem, -0.7rem);}
+        100% {transform: translate(0rem, 0rem);}
+    }
+
+    @keyframes idle43 {
+        0% {transform: translate(0rem, 0rem) rotate(-32.561804deg);}
+        25% {transform: translate(1rem, 0.5rem) rotate(-32.561804deg);}
+        40% {transform: translate(1rem, -0.5rem) rotate(-32.561804deg);}
+        50% {transform: translate(2rem, 0.5rem) rotate(-32.561804deg);}
+        60% {transform: translate(2rem, 1rem) rotate(-32.561804deg);}
+        75% {transform: translate(-1rem, -0.5rem) rotate(-32.561804deg);}
+        100% {transform: translate(0rem, 0rem) rotate(-32.561804deg);}
+    }
+
+    @keyframes idle53 {
+        0% {transform: translate(0rem, 0rem) rotate(36.002129deg);}
+        25% {transform: translate(1rem, 0.5rem) rotate(36.002129deg);}
+        40% {transform: translate(1rem, -0.5rem) rotate(36.002129deg);}
+        50% {transform: translate(2rem, 0.5rem) rotate(36.002129deg);}
+        60% {transform: translate(2rem, 1rem) rotate(36.002129deg);}
+        75% {transform: translate(-1rem, -0.5rem) rotate(36.002129deg);}
+        100% {transform: translate(0rem, 0rem) rotate(36.002129deg);}
+    }
+    </style>
