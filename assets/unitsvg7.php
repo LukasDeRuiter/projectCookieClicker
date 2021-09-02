fe8a28e1c188c2c03313ed280e5f15a12cd4be12
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-09-02 22:17:57 +0200

    added cannister svg

---
+++ assets/unitsvg7.php
@@ -0,0 +1,137 @@
+<svg
+   width="100%"
+   height="100%"
+   viewBox="0 0 210 297"
+   version="1.1"
+   id="svg1937"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs
+     id="defs1934" />
+  <g
+     id="layer1">
+    <ellipse class="bottom18"
+       style="fill:#808080;stroke:#808080;stroke-width:1.68536"
+       id="path2025-1"
+       cx="106.34003"
+       cy="276.87793"
+       rx="63.370548"
+       ry="18.077579" />
+    <rect class="body18"
+       style="fill:#808080;stroke:#808080;stroke-width:5.79226"
+       id="rect2171"
+       width="124.35194"
+       height="164.24452"
+       x="44.131657"
+       y="112.94221"
+       rx="4.0104632"
+       ry="6.3358092" />
+    <ellipse class="top18"
+       style="fill:#333333;stroke:#808080;stroke-width:1.6955"
+       id="path2025"
+       cx="106.1431"
+       cy="112.83858"
+       rx="64.153183"
+       ry="18.072512" />
+    <ellipse class="eye18"
+       style="fill:#000000;stroke:#808080;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path2338"
+       cx="76.604195"
+       cy="183.535"
+       rx="15.1633"
+       ry="20.874153" />
+    <ellipse class="eye28"
+       style="fill:#000000;stroke:#808080;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path2338-2"
+       cx="125.63877"
+       cy="183.92885"
+       rx="15.1633"
+       ry="20.874153" />
+    <path class="mouth18"
+       style="fill:#800000;stroke:#808080;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path2482"
+       d="m 137.84818,229.41876 a 34.46204,22.252634 0 0 1 -17.23102,19.27135 34.46204,22.252634 0 0 1 -34.462042,0 34.46204,22.252634 0 0 1 -17.231019,-19.27135 h 34.462041 z" />
+    <rect class="eyebrow28"
+       style="fill:#000000;stroke:#808080;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect2604"
+       width="44.89912"
+       height="11.421706"
+       x="48.443787"
+       y="148.08833"
+       rx="0.59699792"
+       ry="8.1755352" />
+    <rect class="eyebrow18"
+       style="fill:#000000;stroke:#808080;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect2604-9"
+       width="44.89912"
+       height="12.603262"
+       x="106.34003"
+       y="140.60513"
+       rx="0.59699792"
+       ry="9.0212793" />
+    <ellipse class="eyelid18"
+       style="fill:#f9f9f9;stroke:#808080;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path3074"
+       cx="79.36116"
+       cy="178.02107"
+       rx="6.1047049"
+       ry="7.089335" />
+    <ellipse class="eyelid28"
+       style="fill:#f9f9f9;stroke:#808080;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path3074-6"
+       cx="127.60803"
+       cy="178.80879"
+       rx="6.1047049"
+       ry="7.089335" />
+    <ellipse class="moustache18"
+       style="fill:#784421;stroke:#808080;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path3353"
+       cx="86.253578"
+       cy="218.19397"
+       rx="14.966373"
+       ry="6.301631" />
+    <ellipse class="moustache28"
+       style="fill:#784421;stroke:#808080;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path3353-7"
+       cx="112.64165"
+       cy="218.19397"
+       rx="14.966373"
+       ry="6.301631" />
+  </g>
+</svg>
+
+<style>
+
+.body18{
+   animation: idle18 5s infinite;
+}
+
+.top18{
+   animation: idle28 5s infinite;
+}
+
+.eye18, .eye28 , .eyelid18, .eyelid28, .mouth18, .moustache18, .moustache28 , .eyebrow18, .eyebrow28{
+   animation: idle38 5s infinite;
+}
+
+@keyframes idle18 {
+   0% {transform: scaleY(1);}
+   50% {transform: scaleY(0.6) translateY(65%);}
+   100% {transform: scaleY(1);}
+}
+
+@keyframes idle28 {
+   0% {transform: translateY(0%);}
+   40% {transform: translateY(25%);}
+   57% {transform: translateY(25%);}
+   100% {transform: translateY(0%);}
+}
+
+@keyframes idle38 {
+   0% {transform: translateY(0%);}
+   40% {transform: translateY(15%);}
+   57% {transform: translateY(15%);}
+   100% {transform: translateY(0%);}
+}
+
+</style>
