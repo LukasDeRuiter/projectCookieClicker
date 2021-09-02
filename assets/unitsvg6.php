e302be8cec75428a64ce2868e609a302f08efaef
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-09-02 22:16:21 +0200

    added robot svg

---
+++ assets/unitsvg6.php
@@ -0,0 +1,199 @@
+<svg
+   width="100%"
+   height="100%"
+   viewBox="0 0 210 297"
+   version="1.1"
+   id="svg5"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs
+     id="defs2" />
+  <g
+     id="layer1">
+    <rect class="body17"
+       style="fill:#808080;stroke:#4d4d4d;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect868"
+       width="63.063328"
+       height="102.84827"
+       x="69.242691"
+       y="130.06061"
+       rx="10.026955"
+       ry="22.855167" />
+    <ellipse class="wheel17"
+       style="fill:#999999;stroke:#000000;stroke-width:9.37457;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path981"
+       cx="102.04855"
+       cy="266.60577"
+       rx="26.498529"
+       ry="26.734655" />
+    <rect class="spoke17"
+       style="fill:#666666;stroke:#333333;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect1107"
+       width="8.6564474"
+       height="49.550705"
+       x="97.310448"
+       y="216.11278"
+       rx="0.59699661"
+       ry="22.855158" />
+    <rect class="head17"
+       style="fill:#808080;stroke:#4d4d4d;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect1131"
+       width="61.860409"
+       height="42.302116"
+       x="70.445595"
+       y="87.758492"
+       rx="0.59699792"
+       ry="17.718327" />
+    <rect class="eye17"
+       style="fill:#00ff00;stroke:#000000;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect1133"
+       width="14.029421"
+       height="12.835427"
+       x="80.594551"
+       y="97.310448"
+       rx="0.59699792"
+       ry="12.835427" />
+    <rect class="eye27"
+       style="fill:#00ff00;stroke:#000000;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect1133-7"
+       width="14.029421"
+       height="12.835427"
+       x="106.11615"
+       y="97.161201"
+       rx="0.59699792"
+       ry="12.835427" />
+    <rect class="mouth17"
+       style="fill:#333333;stroke:#000000;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect1179"
+       width="37.31229"
+       height="5.6714678"
+       x="81.490036"
+       y="118.50383"
+       rx="0.59699792"
+       ry="5.6714678" />
+    <rect class="sensorRect17"
+       style="fill:#000000;stroke:#000000;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect1181"
+       width="5.9699674"
+       height="11.939931"
+       x="97.60894"
+       y="76.415565"
+       rx="0.59699792"
+       ry="5.9699655" />
+    <ellipse class="sensor17"
+       style="fill:#ff0000;stroke:#000000;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path1227"
+       cx="99.996941"
+       cy="68.505363"
+       rx="8.6564512"
+       ry="8.208703" />
+    <ellipse class="sensor27"
+       style="fill:#ffffff;stroke:#000000;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path1251"
+       cx="102.83266"
+       cy="66.117371"
+       rx="2.8357339"
+       ry="2.537236" />
+    <ellipse class="eyelid17"
+       style="fill:#ffffff;stroke:#000000;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path1251-2"
+       cx="89.847992"
+       cy="102.08643"
+       rx="2.8357339"
+       ry="2.537236" />
+    <ellipse class="eyelid27"
+       style="fill:#ffffff;stroke:#000000;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none"
+       id="path1251-2-8"
+       cx="115.22035"
+       cy="102.08642"
+       rx="2.8357339"
+       ry="2.537236" />
+    <rect class="bodyDetail17"
+       style="fill:#4d4d4d;stroke:#4d4d4d;stroke-width:1.34039;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect1591"
+       width="2.5414352"
+       height="100.8148"
+       x="106.48717"
+       y="130.89662"
+       rx="0"
+       ry="26.526114" />
+    <rect class="bodyDetail27"
+       style="fill:#4d4d4d;stroke:#4d4d4d;stroke-width:0.84587;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect1591-9"
+       width="2.5414217"
+       height="40.14875"
+       x="165.08592"
+       y="-109.6759"
+       rx="0"
+       ry="10.563829"
+       transform="matrix(-0.00142055,0.99999899,-0.99995988,-0.00895722,0,0)" />
+    <rect class="bodyDetail37"
+       style="fill:#4d4d4d;stroke:#4d4d4d;stroke-width:0.653636;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect1591-9-5"
+       width="2.54142"
+       height="23.973797"
+       x="149.14749"
+       y="-133.04848"
+       rx="0"
+       ry="0"
+       transform="matrix(-8.4818414e-4,0.99999964,-0.99988748,-0.01500059,0,0)" />
+    <rect class="bodyDetail47"
+       style="fill:#333333;stroke:#4d4d4d;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect1909"
+       width="5.9699664"
+       height="5.074471"
+       x="114.62335"
+       y="158.50259"
+       rx="0.59699792"
+       ry="5.074471" />
+    <rect class="bodyDetail57"
+       style="fill:#333333;stroke:#4d4d4d;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none"
+       id="rect1911"
+       width="6.5669627"
+       height="5.3729696"
+       x="95.220955"
+       y="155.51762"
+       rx="0.59699792"
+       ry="5.07447" />
+  </g>
+</svg>
+
+<style>
+
+.wheel17, .body17, .bodyDetail17, .bodyDetail27, .bodyDetail37, .bodyDetail47, .bodyDetail57, 
+.sensorRect17, .sensor17, .sensor27, .spoke17, .mouth17, .head17{
+   animation: idle17 10s infinite linear;
+}
+
+.eye17, .eye27, .eyelid17, .eyelid27{
+   animation: idle27 10s infinite linear;
+}
+
+
+@keyframes idle17 {
+   0% {transform: translateX(0%);}
+   20% {transform: translateX(-25%);}
+   30% {transform: translateX(-25%);}
+   70% {transform: translateX(35%);}
+   80% {transform: translateX(35%);}
+   100% {transform: translateX(0%);}
+}
+
+
+@keyframes idle27 {
+   0% {transform: translateX(0%);}
+   20% {transform: translateX(-25%);}
+   21% {transform: translate(-30%, 2%);
+      fill: red;}
+   29% {transform: translate(-30%, 2%);
+      fill: red;}
+   30% {transform: translate(-25%, 0%);}
+   70% {transform: translateX(35%);}
+   71% {transform: translate(40%, -2%);
+      fill: red;}
+   79% {transform: translate(40%, -2%);
+      fill: red;}
+   80% {transform: translate(35%, 0%);}
+   100% {transform: translateX(0%);}
+}
+</style>
