5aac953261f6823c8814bb9fcdd26afcfeb960c1
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-09-03 12:15:26 +0200

    added new lands

--- createNewUnits.js
+++ createNewUnits.js
@@ -5,9 +5,9 @@ units[1] = new Unit("Brock", 16, 100, 0 , 16000, "Brocks home", 500, 2, false);
 units[2] = new Unit("Peeper", 20, 200, 0 , 30000, "Peepers home", 500, 3, false);
 units[3] = new Unit("Ernest", 30, 300, 0 , 40000, "Ernests home", 500, 4, false);
 units[4] = new Unit("Old man Jenkins", 35, 400, 0 , 45000, "Jenkins home", 500, 5, false);
-units[5] = new Unit("Raccoon", 40, 500, 0 , 50000, "Raccoon home", 500, 1, false);
-units[6] = new Unit("Roboboy", 45, 600, 0 , 60000, "Roboboy home", 500, 1, false);
-units[7] = new Unit("Cannister", 50, 700, 0 , 65000, "Cannister home", 500, 1, false);
-units[8] = new Unit("Rocky", 55, 800, 0 , 65000, "Rocky home", 500, 1, false);
+units[5] = new Unit("Raccoon", 40, 500, 0 , 50000, "Raccoon home", 500, 6, false);
+units[6] = new Unit("Roboboy", 45, 600, 0 , 60000, "Roboboy home", 500, 7, false);
+units[7] = new Unit("Cannister", 50, 700, 0 , 65000, "Cannister home", 500, 8, false);
+units[8] = new Unit("Rocky", 55, 800, 0 , 65000, "Rocky home", 500, 9, false);
 
 let counterCookies = 0;
